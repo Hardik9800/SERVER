ccdcc10bf5b7c32790acb8aec1595b8eb1378c66
Hardik9800 <51282682+Hardik9800@users.noreply.github.com>
2024-02-19 22:23:03 +0530

    Enable All CORS request error solved

--- index.js
+++ index.js
@@ -28,7 +28,14 @@ const app = express();
 const server = http.createServer(app);
 app.use(express.json());
 app.use(express.urlencoded());
-
+app.use(cors(), function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 
 
